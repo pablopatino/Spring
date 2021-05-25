a9a9cb97b0b16b0cb995925169c7e3f7e9777a7c
pablopatino <pablopatino97@hotmail.com>
2021-05-25 16:22:13 -0500

    Spring.

---
+++ src/main/java/com/web/app/controllers/ParamsController.java
@@ -0,0 +1,53 @@
+package com.web.app.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/params")
+public class ParamsController {
+
+	@GetMapping("/string")
+	public String param(@RequestParam(defaultValue = "No ha ingresado algun valor") String texto,  Model model) {
+		
+		model.addAttribute("resultado", "El texto enviado al parametro es " + texto);
+		
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo ,@RequestParam Integer numero,  Model model) {
+		
+		model.addAttribute("resultado", "El texto enviado al parametro es " + saludo + " Y El numero es: " + numero);
+		
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params2")
+	public String param(HttpServletRequest request,  Model model) {
+		 String saludo = request.getParameter("saludo");
+		 Integer numero = null;
+		 try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+		model.addAttribute("resultado", "El texto enviado al parametro es " + saludo + " Y El numero es: " + numero);	
+		return "params/ver";
+	}
+	
+	
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "params/index";
+	}
+	
+	
+}
